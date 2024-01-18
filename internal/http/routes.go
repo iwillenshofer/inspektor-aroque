68ad68848a0347e4d54cf1a84eb9c32667fba3bd
iwillens <iwillens@student.42sp.org.br>
2024-01-18 11:11:42 -0300

    --amend

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -11,7 +11,7 @@ import (
 func RegisterRoutes(r *mux.Router, lg *zap.SugaredLogger) {
 	handler := handlers.NewHandler(lg)
 
-	// register root route am calll inspect
+	// register root route to call inspect
     r.HandleFunc("/", handler.Get()).Methods(http.MethodGet)
 
 	s := r.PathPrefix("/v1").Subrouter()
